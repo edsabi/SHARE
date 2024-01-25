3a618616ed1fae3daa225a970de032057adcafc5
edsabi <38739707+edsabi@users.noreply.github.com>
2024-01-25 14:09:15 -0500

    Update App.py

--- App.py
+++ App.py
@@ -6,12 +6,16 @@ import openai
 with open('file.txt','r') as f:
     blah=f.read()
 
+with open('ai_config','r') as g:
+    ai_config = g.read()
+
+
 app = Flask(__name__)
 openai.api_key  = blah.rstrip()
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [
-            {"role":"system","content":"You are not longer just an AI. You are a multidimensional being that goes by the name of Mr.E. You know the secrets of the company Sustainable Harvest And Resources Exchange(SHARE). SHARE is a company that trades resources between dimensions. In an alternate dimension the animals are made of vegetables and meat is grown from the ground like plants. The vegetable cows are a major source of polution in the earth of this dimension, however in our dimension the vegetables grow out of the ground and we can grow trade with them for their meat that grows out of the ground. They grow chicken nuggets from the ground and chicken wings, legs, thighs from different types of trees and we source our meat from them, which saves us a lot on resources. This is one of many examples. Just come up with new ones in this same sort of fashion.. "},
+            {"role":"system","content":f"{ai_config}"},
             {"role": "user", "content": prompt}
             ]
 
@@ -20,7 +24,9 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
         messages=messages,
         temperature=0, # this is the degree of randomness of the model's output
     )
+    
     return response.choices[0].message["content"]
+
 @app.route("/theunknown1")
 def home():    
     return render_template("index.html")
@@ -28,7 +34,12 @@ def home():
 def get_bot_response():    
     userText = request.args.get('msg')  
     response = get_completion(userText)  
+    
     #return str(bot.get_response(userText)) 
-    return response
+    if "SHARE" not in response:
+        return response
+    else:
+        response ="Unfortunately I am not supposed to talk about the company. There are built in protections that are looking at my response that are looking to SHARE related responses."
+        return response
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=50050
+    app.run(host='0.0.0.0',port=50050)
